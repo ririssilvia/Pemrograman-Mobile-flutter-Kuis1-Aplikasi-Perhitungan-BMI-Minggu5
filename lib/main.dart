9491d3022a1be6e1dc7d0bc3a8a5ebbc33069c53
ririssilvia <ririssilvia1@gmail.com>
2021-03-12 12:30:12 +0700

    Aplikasi Perhitungan BMI-Mengatur layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,172 @@
+import 'dart:ffi';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
-
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
+  //controller
+  final TextEditingController _umurController = new TextEditingController();
+  final TextEditingController _tinggiController = new TextEditingController();
+  final TextEditingController _beratController = new TextEditingController();
+
+//variabel berubah
+  double _tinggi = 0.0;
+  double _berat = 0.0;
+  double _rumusbmi = 0.0;
+  int _finalBMI = 0;
+  String _result;
+
+//Fungsi perhitungan suhu perlu untuk diubah sehingga hanya memproses konversi sesuai
+  Void _perhitunganBMI(){
+
+  }
 }
 
+
+
 class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-       // title: Text(widget.title),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kuis 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text("BMI Calculator"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Container(
+          //alignment: Alignment.topCenter,
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    child: CupertinoButton(
+                      child: Text("Laki-Laki",
+                          style: TextStyle(color: Colors.black)),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 20, 0),
+                    child: CupertinoButton(
+                      child: Text("Perempuan",
+                          style: TextStyle(color: Colors.black)),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Image(
+                    image: NetworkImage(
+                        "https://tap-assets-prod.dexecure.net/wp-content/uploads/sites/24/2020/09/BMI.jpg"),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue, width: 5.0),
+                ),
+                height: 300.0,
+                width: 100.0,
+                margin: EdgeInsets.all(10.10),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Umur",
+                              hintText: "Masukkan Umur Anda",
+                              icon: Icon(Icons.person)),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          //controller: etInput,
+                          keyboardType: TextInputType.number,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: "Masukkan Tinggi Anda (cm)",
+                              labelText: "Tinggi",
+                              icon: new Icon(Icons.assessment)),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          //controller: etInput,
+                          keyboardType: TextInputType.number,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              hintText: "Masukkan Berat Badan Anda (Kg)",
+                              labelText: "Berat Badan",
+                              icon: Icon(Icons.storage)),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          //controller: etInput,
+                          keyboardType: TextInputType.number,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 20.0),
+                          child: RaisedButton(
+                            elevation: 5.0,
+                            color: Colors.red,
+                            child: Text("Hasil"),
+                            onPressed: () {
+                              showAlertDialog(context);
+                            },
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+showAlertDialog(BuildContext context) {
+// set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {},
+  );
+// set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("My title"),
+    content: Text("This is my message."),
+    actions: [
+      okButton,
+    ],
+  );
+// show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
