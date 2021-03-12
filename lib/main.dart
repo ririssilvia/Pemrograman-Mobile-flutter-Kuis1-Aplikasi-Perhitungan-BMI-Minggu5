c5de18bee6b2ccdfab511f5ec2c305ee620849a1
ririssilvia <ririssilvia1@gmail.com>
2021-03-12 23:12:01 +0700

    Aplikasi Perhitungan BMI-keterangan di setiap baris

--- lib/main.dart
+++ lib/main.dart
@@ -5,206 +5,154 @@ void main() {
   runApp(MyApp());
 }
 
+//menggunakan stateful widget akan terjadi perubahan saat fungsi set state dieksekusi/dinamis datanya
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+
 }
 
 class _MyAppState extends State<MyApp> {
-  //controller
+  //membuat controller untuk mengisikan  form/textfield yang dapat diedit/diisi
   final TextEditingController _jkController = new TextEditingController();
   final TextEditingController _umurController = new TextEditingController();
   final TextEditingController _tinggiController = new TextEditingController();
   final TextEditingController _beratController = new TextEditingController();
 
-  //variabel berubah
+  //deklarasi variabel yang bisa berubah nilainya
   double _tinggi = 0.0;
   double _berat = 0.0;
   double _rumushasilBMI = 0.0;
   int _finalBMI = 0;
   String _result;
 
-  //Fungsi perhitungan suhu perlu untuk diubah sehingga hanya memproses konversi sesuai
-  //dengan pilihan pengguna.
-  void _perhitunganBmi() {
-     _berat = double.parse(_beratController.text);
-      _tinggi = double.parse(_tinggiController.text);
-     
-    setState(() {
-       _rumushasilBMI = _berat / (_tinggi * _tinggi);
-      _finalBMI = _rumushasilBMI.round();
+  
+  void _perhitunganBmi() { //fungsi untuk menghitung BMI
+    _berat = double.parse(_beratController.text);//mengisi variabel berat sesuai niali yang di tampung pada Controller
+    _tinggi = double.parse(_tinggiController.text);////mengisi variabel tinggi sesuai niali yang di tampung pada Controller
+     //berat dan tinggi ada di luar setstate karena ditampilakan di ui jika yang di dalam setstate tidak ditampilkan kedalam ui
+    setState(() { //fungsi akan di panggil setiap ada perubahan
+       _rumushasilBMI = _berat / (_tinggi * _tinggi);//rumus perhitungan BMI
+      _finalBMI = _rumushasilBMI.round();//hasil dari perhitungan BMI yang di bulatkan
      
 
-      if (_finalBMI <= 18) {
-        _result = "Anda kekurangan berat badan";
-      } else if (_finalBMI > 18 && _finalBMI <= 25) {
-        _result = "Anda sempurna";
-      } else if (_finalBMI > 25 && _finalBMI <= 30) {
-        _result = "Anda kelebihan berat badan";
-      } else if (_finalBMI > 30) {
-        _result = "Anda obesitas";
+      if (_finalBMI <= 18) {//kondisi jika finalbmi bernilai <=18 
+        _result = "Anda kekurangan berat badan"; //maka akan di tampilkan kondisi seperti ini
+      } else if (_finalBMI > 18 && _finalBMI <= 25) {//jika kondisi yang pertama tidak terpenuhi maka akan turun ke kondisi selanjutnya dengan nilai lebih dari 18 sampai dengan kurang samandenagn25
+        _result = "Anda sempurna";//maka akan di tampilkan kondisi seperti ini
+      } else if (_finalBMI > 25 && _finalBMI <= 30) { //jika kondisi keduanya tadi tidak terpenuhi maka akan meng eksukusi kondisi ini dengan nilai _finalBMI > 25 && _finalBMI <= 30
+        _result = "Anda kelebihan berat badan";//maka akan di tampilkan kondisi seperti ini
+      } else if (_finalBMI > 30) {//jika kondisi ketiganya tidak terpenuhi maka akan mengesekusi kondisi ini dengan nilai final bmi lebih dari 30
+        _result = "Anda obesitas";//maka akan di tampilkan kondisi seperti ini
       }
     });
   }
-  // void _tampilkanalert() {
-  //   AlertDialog alertDialog = new AlertDialog(
-  //     content: new Container(
-  //       height: 200.0,
-  //       child:  Center(
-  //         child:  Text(
-  //           " Hasil BMI Anda  is $_finalBMI : $_result"),
-  //       ),
-  //     ),
-  //     actions: [
-  //       // ignore: deprecated_member_use
-  //       FlatButton(
-  //         child: Text('Tutup'),
-  //         onPressed: () {
-  //           Navigator.of(context).pop();
-  //         },
-  //       ),
-  //     ],
-  //   );
-  //   //showDialog(context: context, child: alertDialog, barrierDismissible: false,);
-  // }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {//widget yang dijalankan saat dibuild
+    // titik awal aplikasi  ini memberi tahu Flutter bahwa akan menggunakan komponen Material dan mengikuti desain material di aplikasi .
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kuis 1',
+      debugShowCheckedModeBanner: false,//untuk menghilangkan debug di sebelah pojok kanan atas
+      title: 'Kuis 1',//judul untuk aplikasi 
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        visualDensity: VisualDensity.adaptivePlatformDensity,//mengatur tampilan agar sesuai dengan platform yang dirun
       ),
+      //Scaffolddigunakan di bawah MaterialApp, memberikan Anda banyak fungsi dasar, seperti AppBar, BottomNavigationBar, Drawer, FloatingActionButtondll
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.home),
-          title: Text("BMI Calculator"),
-          backgroundColor: Colors.blue,
-          centerTitle: true,
+        appBar: AppBar(//menagtur bagian atas aplikasi
+          leading: Icon(Icons.home),//memberikan icon home
+          title: Text("BMI Calculator"),//memeberi judul di bagian atas aplikasi
+          backgroundColor: Colors.blue,//memberikan warna biru untuk aplikasi bagaian atas atau appbar 
+          centerTitle: true,//dengan judul di tengah
         ),
-        body: Container(
-          //alignment: Alignment.topCenter,
-          child: ListView(
+        body: Container(//membuat pengaturan untuk bagian utama halaman yaitu body
+          child: ListView(//untuk membiat layout berbentuk listviw
             children: [
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: [
-              //     Container(
-              //       margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
-              //       child: CupertinoButton(
-              //         child: Text("Laki-Laki",
-              //             style: TextStyle(color: Colors.black)),
-              //         onPressed: () {},
-              //       ),
-              //     ),
-              //     Container(
-              //       margin: EdgeInsets.fromLTRB(10, 10, 20, 0),
-              //       child: CupertinoButton(
-              //         child: Text("Perempuan",
-              //             style: TextStyle(color: Colors.black)),
-              //         onPressed: () {},
-              //       ),
-              //     ),
-              //   ],
-              // ),
               Container(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Image(
-                    image: NetworkImage(
+                child: Padding(//untuk memberikan widget child yang ada di dalam dengan memeberikan jarak 
+                  padding: EdgeInsets.only(top: 10.0),//mrngatur padding di bagian tertentu yaitu di bagain atas saja
+                  child: Image(//chlid dari padding yaitu gambar
+                    image: NetworkImage( //untuk mengambil gambar dari internet
                         "https://tap-assets-prod.dexecure.net/wp-content/uploads/sites/24/2020/09/BMI.jpg"),
                   ),
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blue, width: 5.0),
+              Container(//widget container untuk mengatur  layout child
+                decoration: BoxDecoration( //mengatur border
+                  border: Border.all(color: Colors.blue, width: 5.0), //mengatur ketebalan border
                 ),
-                height: 400.0,
-                width: 100.0,
-                margin: EdgeInsets.all(10.10),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                height: 400.0,//mengatur panjang border
+                width: 100.0,//mengatur lebar border
+                margin: EdgeInsets.all(10.10),//mengatur margin atau batas keseluruhan
+                child: Padding(//mengatur padding
+                  padding:  EdgeInsets.all(16.0),//mengatur padding atau batas keseluruhan
+                  child: Column(//membuat widget chlid column
+                      mainAxisAlignment: MainAxisAlignment.center,//membuat column agar di tengah
                       children: [
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Jenis Kelamain",
-                              hintText: "Masukkan Jenis Kelamin Anda",
-                              icon: Icon(Icons.person_search_rounded)),
-                          controller: _jkController,
+                        TextField(//widged texfielde untuk sebuah inputan dari user berupa text
+                          decoration: InputDecoration(//mengatur tambpilan dibagian input
+                              labelText: "Jenis Kelamain",//untuk memberi label pada textfield
+                              hintText: "Masukkan Jenis Kelamin Anda",//hint teks digunakan untuk memberi petunjuk pada textFormFieldnya
+                              icon: Icon(Icons.person_search_rounded),//memberikan icon pada inputdecoration
+                              ),
+                          controller: _jkController,//controller digunakan untuk menyimpan inputan jenis kelamin 
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Umur",
-                              hintText: "Masukkan Umur Anda",
-                              icon: Icon(Icons.person)),
-                          inputFormatters: [
+                        TextField( //widged texfielde untuk sebuah inputan dari user berupa text
+                          decoration: InputDecoration(//mengatur tambpilan dibagian input
+                              labelText: "Umur",//untuk memberi label pada textfield
+                              hintText: "Masukkan Umur Anda",//hint teks digunakan untuk memberi petunjuk pada textFormFieldnya
+                              icon: Icon(Icons.person),//memberikan icon pada inputdecoration
+                              ),
+                          inputFormatters: [//inputan khusus format digital onlny yaitu 0-9
                             FilteringTextInputFormatter.digitsOnly
                           ],
-                          controller: _umurController,
-                          keyboardType: TextInputType.number,
+                          controller: _umurController,//controller digunakan untuk menyimpan inputan umur
+                          keyboardType: TextInputType.number,//dengan type inputan keyboard nomor
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                              hintText: "Masukkan Tinggi Anda (m)",
-                              labelText: "Tinggi",
-                              icon: new Icon(Icons.assessment)),
-                         
-                          controller: _tinggiController,
-                          keyboardType: TextInputType.number,
+                        TextField(//widged texfielde untuk sebuah inputan dari user berupa text
+                          decoration: InputDecoration(//mengatur tambpilan dibagian input
+                              hintText: "Masukkan Tinggi Anda (m)",//hint teks digunakan untuk memberi petunjuk pada textFormFieldnya
+                              labelText: "Tinggi",//untuk memberi label pada textfield
+                              icon: new Icon(Icons.assessment), //memberikan icon 
+                              ),
+                          controller: _tinggiController,//controller digunakan untuk menyimpan inputan tinggi
+                          keyboardType: TextInputType.number,//dengan type inputan keyboard nomor
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                              hintText: "Masukkan Berat Badan Anda (Kg)",
-                              labelText: "Berat Badan",
-                              icon: Icon(Icons.storage)),
-                          inputFormatters: [
+                        TextField(//widged texfielde untuk sebuah inputan dari user berupa text
+                          decoration: InputDecoration(//mengatur tambpilan dibagian input
+                              hintText: "Masukkan Berat Badan Anda (Kg)",//hint teks digunakan untuk memberi petunjuk pada textFormFieldnya
+                              labelText: "Berat Badan",//untuk memberi label pada textfield
+                              icon: Icon(Icons.storage),//untuk memberi icon
+                              ),
+                          inputFormatters: [//inputan khusus format digital onlny yaitu 0-9
                             FilteringTextInputFormatter.digitsOnly
                           ],
-                          controller: _beratController,
-                          keyboardType: TextInputType.number,
+                          controller: _beratController,//controller digunakan untuk menyimpan inputan berat
+                          keyboardType: TextInputType.number,//dengan type inputan keyboard nomor
                         ),
-                        // Container(
-                        //   padding: EdgeInsets.all(10.0),
-                        //   child: Center(
-                        //     child: RaisedButton(
-                        //       child: Text(
-                        //         "Kalkulasi",
-                        //         style: TextStyle(color: Colors.white),
-                        //       ),
-                        //       color: Colors.blue,
-                        //       onPressed: () {
-                        //        _tampilkanalert();
-                        //       },
-                        //     ),
-                        //   ),
-                        // ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 20.0),
+                        Container(//widgwd container untuk mengatur button
+                          margin:  EdgeInsets.only(top: 20.0),//mengatur margin hanya bagaian atas
                           // ignore: deprecated_member_use
-                          child: RaisedButton(
-                          onPressed: _perhitunganBmi,
-                          color: Colors.teal,
-                          child: Text(
+                          child: RaisedButton(//widgwd child button
+                          onPressed: _perhitunganBmi,//untuk proses ketika di klik maka fungsi _perhitungan bmi akan di eksekusi
+                          color: Colors.teal,//memberikan warna pada button
+                          child: Text(//memberikan text pada button dengan nama calculate
                             "Calculate",
-                            style: TextStyle(
-                                color: Colors.white, fontSize: 16.9),
+                            style: TextStyle(//memberikan text dengan style warna putih dengan besarnya 
+                                color: Colors.white, fontSize: 17),
                           ),
                              ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Center(
-                            child: Text
-                            ("BMI Anda Adalah $_finalBMI : $_result",
-                            style: TextStyle(
+                        Padding(//widget padding
+                          padding: EdgeInsets.only(top: 5.0),//mengatur padding kushus bagaian atas
+                          child: Center(//membuad chlid di bagain tengah dengan berisikan text
+                            child: Text//widget txt
+                            ("BMI Anda Adalah $_finalBMI : $_result",//untuk menampilkan berupa hasil final dan result di dalam kondisi
+                            style: TextStyle(//memeberikan style dengan warna biru dengan font 18 dan di bold
                               color: Colors.blueAccent,
                               fontSize: 18.0,
-                              fontWeight: FontWeight.w500
+                              fontWeight: FontWeight.bold
                             ),
                             ),
                             ),
